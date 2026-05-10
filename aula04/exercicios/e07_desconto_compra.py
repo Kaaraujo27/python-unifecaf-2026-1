1bd76510d24d6766ac300f730a14072c655eaa89
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:04:41 -0300

    Update e07_desconto_compra.py

--- aula04/exercicios/e07_desconto_compra.py
+++ aula04/exercicios/e07_desconto_compra.py
@@ -2,3 +2,11 @@
 # Leia o valor de uma compra. Se for maior que 100, aplique 10% de desconto.
 
 # escreva seu código abaixo
+
+valor_compra = float(input("Digite o valor da compra: "))
+if valor_compra > 100:
+    desconto = valor_compra * 0.10
+    valor_com_desconto = valor_compra - desconto
+    print(f"O valor da compra com desconto é: R$ {valor_com_desconto:.2f}")
+else:
+    print(f"O valor da compra é: R$ {valor_compra:.2f} (sem desconto)")
