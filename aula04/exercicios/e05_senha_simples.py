1b7087d81813ca278ba194bde4763d5767f7eb88
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:02:28 -0300

    Update e05_senha_simples.py

--- aula04/exercicios/e05_senha_simples.py
+++ aula04/exercicios/e05_senha_simples.py
@@ -2,3 +2,9 @@
 # Peça uma senha e verifique se ela é igual a '1234'.
 
 # escreva seu código abaixo
+
+senha = input("Digite a senha: ")
+if senha == '1234':
+    print("Senha correta.")
+else:
+    print("Senha incorreta.")
