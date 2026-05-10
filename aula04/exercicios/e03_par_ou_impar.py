93f25e3eac601fecfcbbec34e6784ccefd531c3b
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:02:23 -0300

    Update e03_par_ou_impar.py

--- aula04/exercicios/e03_par_ou_impar.py
+++ aula04/exercicios/e03_par_ou_impar.py
@@ -2,3 +2,9 @@
 # Leia um número inteiro e informe se ele é par ou ímpar.
 
 # escreva seu código abaixo
+
+numero = int(input("Digite um número inteiro: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
