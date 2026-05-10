e3f7edcff75980fcfc9c6c33dba2c95bd21ad730
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:02:19 -0300

    Update e01_verificar_maior_idade.py

--- aula04/exercicios/e01_verificar_maior_idade.py
+++ aula04/exercicios/e01_verificar_maior_idade.py
@@ -2,3 +2,9 @@
 # Peça a idade do usuário e informe se ele é maior ou menor de idade.
 
 # escreva seu código abaixo
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
