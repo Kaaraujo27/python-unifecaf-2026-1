7517957e852fc8d7d95262b8f152790034c56837
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 16:51:43 -0300

    Update e03_somar_todos_os_numeros.py

--- aula03/exercicios/e03_somar_todos_os_numeros.py
+++ aula03/exercicios/e03_somar_todos_os_numeros.py
@@ -3,3 +3,10 @@
 # dicas:
 # - estrutura do while: while condição:
 # - use input para ler o número n
+
+n = int(input("Digite um número (0 para sair): "))
+soma = 0
+while n != 0:
+    soma += n
+    n = int(input("Digite um número (0 para sair): "))
+print("A soma de todos os números digitados é:", soma)
