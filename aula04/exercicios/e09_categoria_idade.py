25d68817da0669b40fd9f0a062a1a6265717599b
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 20:40:07 -0300

    Update e09_categoria_idade.py

--- aula04/exercicios/e09_categoria_idade.py
+++ aula04/exercicios/e09_categoria_idade.py
@@ -2,3 +2,11 @@
 # Leia a idade e classifique: criança (<12), adolescente (<18), adulto.
 
 # escreva seu código abaixo
+
+idade = int(input('Digite a idade: '))
+if idade < 12:
+    print('Criança')
+elif idade < 18:
+    print('Adolescente')
+else: 
+    print('Adulto')
