8ea3805a54874b353a9b197009d15785c02ea680
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:04:54 -0300

    Update e08_temperatura_clima.py

--- aula04/exercicios/e08_temperatura_clima.py
+++ aula04/exercicios/e08_temperatura_clima.py
@@ -2,3 +2,11 @@
 # Leia uma temperatura e classifique: frio (<15), ameno (15-25), quente (>25).
 
 # escreva seu código abaixo
+
+temperatura = float(input("Digite a temperatura em graus Celsius: "))
+if temperatura < 15:
+    print("A temperatura é classificada como: Frio")
+elif 15 <= temperatura <= 25:
+    print("A temperatura é classificada como: Ameno")
+else:
+    print("A temperatura é classificada como: Quente")
