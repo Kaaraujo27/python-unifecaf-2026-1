b55a75a3741d948e4f3a64da95f75f3dbb508f12
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 16:44:05 -0300

    Update e2_desconto.py

--- aula01/exercicios/e2_desconto.py
+++ aula01/exercicios/e2_desconto.py
@@ -5,3 +5,11 @@
 # Ao final, imprima se o cliente tem direito ao desconto.
 # O cliente tem direito ao desconto se for cliente VIP, mas
 # também pode ter se possuir um cupom de desconto.
+
+cliente_vip = True
+cupom_desconto = False
+
+if cliente_vip or cupom_desconto:
+    print("O cliente tem direito ao desconto.")
+else:
+    print("O cliente não tem direito ao desconto.")
