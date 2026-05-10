93381c0be1ed1ea34f715027dcbcf5e49c85758f
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:02:21 -0300

    Update e02_numero_positivo_negativo.py

--- aula04/exercicios/e02_numero_positivo_negativo.py
+++ aula04/exercicios/e02_numero_positivo_negativo.py
@@ -2,3 +2,11 @@
 # Leia um número e informe se ele é positivo, negativo ou zero.
 
 # escreva seu código abaixo
+
+numero = float(input("Digite um número: "))
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
