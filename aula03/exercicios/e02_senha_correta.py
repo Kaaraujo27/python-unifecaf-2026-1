26bb802d3425f60c6324667e6f1a66842ab43e79
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 16:51:40 -0300

    Update e02_senha_correta.py

--- aula03/exercicios/e02_senha_correta.py
+++ aula03/exercicios/e02_senha_correta.py
@@ -1,3 +1,9 @@
 # Peça a senha ao usuário e continue pedindo até ele digitar: unifecaf
 # - estrutura do while: while condição:
 # - use input para ler a senha
+
+senha = input("Digite a senha: ")
+while senha != "unifecaf":
+    print("Senha incorreta, tente novamente.")
+    senha = input("Digite a senha: ") 
+    print("Senha correta, acesso concedido.")
