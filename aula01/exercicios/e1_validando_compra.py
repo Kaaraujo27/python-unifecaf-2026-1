d539ab17f360ca49994c8b1a73ebb03c7ee9692b
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 16:58:33 -0300

    Update e1_validando_compra.py

--- aula01/exercicios/e1_validando_compra.py
+++ aula01/exercicios/e1_validando_compra.py
@@ -7,7 +7,7 @@
 # e se houver produto no estoque
 
 compra_paga = True
-produto_em_estoque = True
+produto_em_estoque = False
 
 if compra_paga and produto_em_estoque:
     print("O produto pode ser vendido.")
