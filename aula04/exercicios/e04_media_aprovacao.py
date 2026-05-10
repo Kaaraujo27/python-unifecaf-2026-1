01f826591d4c7e531c6004324574c6f1caff334e
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-10 17:02:25 -0300

    Update e04_media_aprovacao.py

--- aula04/exercicios/e04_media_aprovacao.py
+++ aula04/exercicios/e04_media_aprovacao.py
@@ -2,3 +2,11 @@
 # Leia duas notas e informe se o aluno foi aprovado (>=7) ou reprovado.
 
 # escreva seu código abaixo
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+media = (nota1 + nota2) / 2
+if media >= 7:
+    print("Aluno aprovado.")
+else:
+    print("Aluno reprovado.")
