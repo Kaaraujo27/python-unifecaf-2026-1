f346634aabb340d1de4e41b9a0e95c2445a3ea43
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-11 18:17:24 -0300

    Update e10_login_usuario.py

--- aula04/exercicios/e10_login_usuario.py
+++ aula04/exercicios/e10_login_usuario.py
@@ -2,3 +2,10 @@
 # Peça usuário e informe se é 'admin'.
 
 # escreva seu código abaixo
+
+usuario = input('Digite seu usuário: ')
+
+if usuario == 'Kayque':
+    print('Bem-Vindo de volta, Kayque!')
+else:
+    print('Acesso negado. Usuário não localizado.')
