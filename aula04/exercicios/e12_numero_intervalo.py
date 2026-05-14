529ea160391460e7ca6df9817bd98c87f2270ae9
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-13 22:11:51 -0300

    Update e12_numero_intervalo.py

--- aula04/exercicios/e12_numero_intervalo.py
+++ aula04/exercicios/e12_numero_intervalo.py
@@ -2,3 +2,11 @@
 # Leia um número e informe se está entre 10 e 20.
 
 # escreva seu código abaixo
+
+def numero_intervalo():
+    numero_entrada = float(input('Digite um número: '))
+    if 10 < numero_entrada < 20:
+        print(f'O número {numero_entrada} está entre 10 e 20.')
+    else: 
+        print(f'O número {numero_entrada} não está entre 10 e 20.')
+numero_intervalo()
