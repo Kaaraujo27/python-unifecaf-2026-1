805eb428055b2905c3fbbaf5a707bb50c6112499
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-13 22:13:59 -0300

    Update e13_tipo_letra.py

--- aula04/exercicios/e13_tipo_letra.py
+++ aula04/exercicios/e13_tipo_letra.py
@@ -2,3 +2,9 @@
 # Leia uma letra e informe se é vogal ou consoante.
 
 # escreva seu código abaixo
+
+letra = input('Digite uma letra: ').lower()
+if letra in 'aeiou':
+    print(f'A letra {letra} é uma vogal.')
+else:
+    print(f'A letra {letra} é uma consoante.')
