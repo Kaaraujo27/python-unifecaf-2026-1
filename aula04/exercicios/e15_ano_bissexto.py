9185a0485e3da250c2dfeee0d98eb977e0c19e3a
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-29 20:17:48 -0300

    Update e15_ano_bissexto.py

--- aula04/exercicios/e15_ano_bissexto.py
+++ aula04/exercicios/e15_ano_bissexto.py
@@ -2,3 +2,9 @@
 # Leia um ano e informe se é bissexto.
 
 # escreva seu código abaixo
+
+dias = input('Quantos dias tem esse ano? ')
+if dias in '366':
+    print('Ano bissexto!')
+else:
+    print('Esse ano não é bissexto!')
