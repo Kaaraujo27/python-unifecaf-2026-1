e1942270b8c7aa40c23ed3ca5832d33ac17cfbf3
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-29 20:17:50 -0300

    Update e16_categoria_produto.py

--- aula04/exercicios/e16_categoria_produto.py
+++ aula04/exercicios/e16_categoria_produto.py
@@ -2,3 +2,12 @@
 # Leia o preço e classifique: barato(<50), médio(<100), caro.
 
 # escreva seu código abaixo
+
+categoria = int(input('Qual é o valor do produto? '))
+if categoria <=50:
+    print('Produto classificado como barato!')
+elif categoria <=100:
+    print('Produto classificado como mediano!')
+else:
+    print('Produto classificado como caro!')
+    
