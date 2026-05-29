48b2edb1066f8004a67b840321a89bb2dd77b277
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-29 20:17:53 -0300

    Update e18_idade_voto.py

--- aula04/exercicios/e18_idade_voto.py
+++ aula04/exercicios/e18_idade_voto.py
@@ -2,3 +2,9 @@
 # Leia idade e informe se pode votar.
 
 # escreva seu código abaixo
+
+idade = int(input('Digite sua idade: '))
+if idade >=18:
+    print('Está permitido votar na eleição! Vote com consciência!!')
+else:
+    print('Você não está permitido para votar ainda!')
