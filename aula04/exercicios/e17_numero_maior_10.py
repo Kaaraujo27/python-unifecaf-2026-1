97d4cc27d433b2ccaa05a5c8c2354ec82c58bc51
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-29 20:17:51 -0300

    Update e17_numero_maior_10.py

--- aula04/exercicios/e17_numero_maior_10.py
+++ aula04/exercicios/e17_numero_maior_10.py
@@ -2,3 +2,9 @@
 # Leia um número e informe se é maior que 10.
 
 # escreva seu código abaixo
+
+numero = int(input('Digite um número: '))
+if numero >10:
+    print('Esse número é maior que dez!')
+else:
+    print('Esse número é menor que dez!')
