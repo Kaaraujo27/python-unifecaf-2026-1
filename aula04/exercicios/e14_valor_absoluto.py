432f601cf3e126a5423b34bf8a762cc7abe44bea
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-05-29 20:20:28 -0300

    Update e14_valor_absoluto.py

--- aula04/exercicios/e14_valor_absoluto.py
+++ aula04/exercicios/e14_valor_absoluto.py
@@ -2,3 +2,8 @@
 # Leia um número e mostre seu valor absoluto.
 
 # escreva seu código abaixo
+
+numero = float(input("Digite um número: "))
+valor_absoluto = abs(numero)
+
+print(f"O valor absoluto de {numero} é {valor_absoluto}")
