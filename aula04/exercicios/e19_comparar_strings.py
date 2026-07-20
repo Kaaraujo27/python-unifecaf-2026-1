1a90d55d041951720e67221aa64c53f263f01cd7
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-07-20 20:14:11 -0300

    Update e19_comparar_strings.py

--- aula04/exercicios/e19_comparar_strings.py
+++ aula04/exercicios/e19_comparar_strings.py
@@ -2,3 +2,11 @@
 # Leia duas palavras e informe se são iguais.
 
 # escreva seu código abaixo
+
+palavra1 = str(input('Digite uma palavra: '))
+palavra2 = str(input('Digite outra palavra: '))
+
+if palavra1 == palavra2:
+    print('As palavras são iguais!')
+else:
+    print('As palavras são diferentes!')
