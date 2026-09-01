4fa8b61ded9d5e25defe5a8617f550ea8eee85af
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-09-01 13:33:32 -0300

    Update e01_somatoria.py

--- aula05/exercicios/e01_somatoria.py
+++ aula05/exercicios/e01_somatoria.py
@@ -1,2 +1,8 @@
 # Crie um algoritmo que leia uma lista de inteiros e retorne a 
 # soma de todos os elementos
+
+lista = [5, 6, 8, 6, 1, 3]
+
+soma = sum(lista)
+
+print(f'A soma dos números na lista é {soma}')
