2384174a7ee6ea13e98726692fb84b981b59a31c
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-09-01 13:33:35 -0300

    Update e06_compare_igualdade_de_lista.py

--- aula05/exercicios/e06_compare_igualdade_de_lista.py
+++ aula05/exercicios/e06_compare_igualdade_de_lista.py
@@ -1,2 +1,15 @@
 # Leia 2 listas e retorne quantos valores existem em. ambas a listas
 # Dica: nesse caso será necessário um for dentro de outro for
+
+lista1 = [1, 3, 5, 6, 8]
+lista2 = [2, 4, 6, 1, 3]
+
+contador = 0
+
+for num1 in lista1:
+    for num2 in lista2:
+        if num1 == num2:
+            contador +=1
+            break
+
+print(f'A quantidade de valores em ambas as listas: {contador}')
