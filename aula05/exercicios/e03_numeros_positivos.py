61b4c44ece02b9864b396c91fd85c19560c956a2
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-09-03 13:49:23 -0300

    Update e03_numeros_positivos.py

--- aula05/exercicios/e03_numeros_positivos.py
+++ aula05/exercicios/e03_numeros_positivos.py
@@ -1,1 +1,10 @@
 # Leia uma lista de inteiros e conte quanto são positivos
+
+numeros = [1, -5, 6, 8, 7, -6, -2, -3, -7]
+contador_positivos = 0
+
+for numero in numeros:
+    if numero >0:
+        contador_positivos +=1
+
+print(f"Quantidade de números positivos: {contador_positivos}")
