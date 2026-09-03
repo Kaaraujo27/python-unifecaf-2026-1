34711db0a5e37d862779a99c9532c387eeebb87f
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-09-03 13:49:21 -0300

    Update e02_retorne_impares.py

--- aula05/exercicios/e02_retorne_impares.py
+++ aula05/exercicios/e02_retorne_impares.py
@@ -1,2 +1,11 @@
 # Crie um aplicativo que leia uma lista de inteiros 
 # e retorne todos os números ímpares
+
+numeros = [1, 2 , 5, 8, 4, 9]
+impares = []
+
+for numero in numeros:
+    if numero % 2 != 0:
+        impares.append(numero)
+
+print(f"Números impares {impares}")
