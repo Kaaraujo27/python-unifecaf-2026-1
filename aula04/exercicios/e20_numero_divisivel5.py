a6d0a4095612e51fa6770d32878cc60b42c37a9b
Kaaraujo27 <kayquearaujo384@gmail.com>
2026-09-03 13:49:20 -0300

    Update e20_numero_divisivel5.py

--- aula04/exercicios/e20_numero_divisivel5.py
+++ aula04/exercicios/e20_numero_divisivel5.py
@@ -2,3 +2,10 @@
 # Leia número e informe se é divisível por 5.
 
 # escreva seu código abaixo
+
+numero = int(input('Digite um número: '))
+
+if numero % 5 == 0:
+    print('Esse número é divisil por 5.')
+else:
+    print('Esse número não é divisil por 5.')
